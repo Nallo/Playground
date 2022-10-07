dd4428859d3380d2fb23bf4ebbe980cc27b69036
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-10-07 11:59:37 +0200

    Add button to request missed calls

--- Playground/ViewController.swift
+++ Playground/ViewController.swift
@@ -11,15 +11,33 @@ class ViewController: UIViewController {
 
     private let placeService = PlaceService()
 
+    private lazy var button: UIButton = {
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("Get Missed Calls".uppercased(), for: .normal)
+        b.addTarget(self, action: #selector(requestMissedCalls), for: .touchUpInside)
+        return b
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .systemMint
+
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
 
         placeService.loadPlaces { places in
             print(places)
         }
     }
 
-
+    @objc func requestMissedCalls() {
+        print(#function)
+    }
 }
 
